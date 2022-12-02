6bff93ff12dc228331a9e9e0dc5ac38b70d32e9b
Chethan-1471 <103055240+Chethan-1471@users.noreply.github.com>
2022-12-02 11:48:15 +0530

    Add files via upload

---
+++ built_in_mathfunction.py
@@ -0,0 +1,39 @@
+#Built-in Math Functions
+#to find min max
+x = min(3, 17, 87)
+y = max(2, 600, 7854)
+print(x)
+print(y)
+#3
+#7854
+
+
+#abs() returns absolute postive value
+x = abs(-7.25)
+print(x)
+#7.25
+
+
+#pow(x,y)
+x = pow(4, 3)
+print(x)        #4*4*4=64
+#64
+
+#math.sqrt()
+import math
+x = math.sqrt(64)
+print(x)
+#8.0
+
+
+
+import math
+x = math.ceil(9.4)        #Round a number upward to its nearest integer
+y = math.floor(9.4)       #Round a number downward to its nearest integer
+print(x)     #returns10
+print(y)     #returns9
+
+
+import math
+x = math.pi   
+print(x)         #returns the value of PI (3.14...)
